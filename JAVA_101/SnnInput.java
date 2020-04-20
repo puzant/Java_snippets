4a4e2018a5b7cf86ac654260922d97602bf646dc
Puzant <PBAKJEJIAN@hotmail.com>
2020-04-20 11:57:38 +0300

    java program to test SNN input

---
+++ JAVA_101/SnnInput.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class SnnInput {
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    String socialSecurityNumber;
+    boolean properLength;
+    boolean properDash;
+
+    System.out.print("Enter the Social Security Number: ");
+
+    socialSecurityNumber = input.nextLine();
+
+    if(socialSecurityNumber.length() != 11) {
+      properLength = false;
+    } else {
+      properLength = true;
+    }
+
+    if((socialSecurityNumber.charAt(4) != '-') && (socialSecurityNumber.charAt(7) != '-')) {
+      properDash = false;
+    } else {
+      properDash = true;
+    }
+
+    if(!properLength && !properDash) {
+      System.out.println("Invalid SNN");
+    } else {
+      System.out.println("Valid SNN");
+    }
+
+
+  }
+
+}
